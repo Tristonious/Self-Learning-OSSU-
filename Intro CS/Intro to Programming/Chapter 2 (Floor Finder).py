6188e76f56b0f262d068317a0e13fd9957fc9e7e
Tristonious <103786963+Tristonious@users.noreply.github.com>
2022-07-03 14:00:57 -0700

    Assignment Ch.2
    Script for recognizing what floor someone is on

---
+++ Intro CS/Intro to Programming/Chapter 2 (Floor Finder).py
@@ -0,0 +1,4 @@
+# This script designates what floor you would be on if you were in the US
+Cur_Floor = input('What floor do you want? ')
+US_Floor= float(Cur_Floor) +1
+print('That would be floor',US_Floor,'in the US.')
