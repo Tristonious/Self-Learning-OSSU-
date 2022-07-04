27de1aeb2455e9893c7610ba909bc98446737385
Tristonious <103786963+Tristonious@users.noreply.github.com>
2022-07-03 21:15:52 -0700

    File Analyzer

---
+++ Intro CS/Intro to Programming/Ch7 (Files).py
@@ -0,0 +1,26 @@
+#Write a program that prompts for a file name, then opens that file and reads through the file, looking for lines of the form:
+#X-DSPAM-Confidence:    0.8475
+#Count these lines and extract the floating point values from each of the lines and compute the average of those values and produce an output as shown below. Do not use the sum() function or a variable named sum in your solution.
+#You can download the sample data at http://www.py4e.com/code3/mbox-short.txt when you are testing below enter mbox-short.txt as the file name.
+
+
+
+
+
+
+fname = input("Enter file name: ")
+fhand = open(fname)
+total=0
+counts=0
+for line in fhand:
+    if not line.startswith("X-DSPAM-Confidence:"):
+        continue
+    elif line.startswith("X-DSPAM-Confidence:"):
+        line= text.find(' ')
+        cut=line[idx1:].strip()
+        Num=float(cut)
+        total=total+Num
+        counts+=1
+    print(line)
+print("Done")
+print("Average spam confidence:",total/counts)
